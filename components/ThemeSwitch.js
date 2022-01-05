b3ffb0f4dfda712e9b790d1dcf5574818e0e9029
JunYeong <skyzuni@nate.com>
2022-01-05 16:03:56 +0900

    darkmode

--- components/ThemeSwitch.js
+++ components/ThemeSwitch.js
@@ -1,14 +1,29 @@
 import React, { useState } from "react";
 import DarkTheme from "./DarkTheme";
 
+const loadDarkMode = () => {
+  if (typeof localStorage === "undefined") {
+    return false;
+  }
+  const value = localStorage.getItem("darkMode");
+  return value === null ? false : JSON.parse(value);
+};
+
 const ThemeSwitch = () => {
-  const [darkMode, setDarkMode] = useState(false);
+  const [darkMode, setDarkMode] = useState(loadDarkMode);
+
+  const handleClick = () => {
+    localStorage.setItem("darkMode", JSON.stringify(!darkMode));
+    setDarkMode(!darkMode);
+  };
   console.log("darkmode: ", darkMode);
 
   const text = darkMode ? "Light Mode" : "Dark Mode";
   return (
     <>
-      <button onClick={() => setDarkMode(!darkMode)}>{text}</button>
+      <button onClick={handleClick} suppressHydrationWarning>
+        {text}
+      </button>
       <style jsx>
         {`
           button {
